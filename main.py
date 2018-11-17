0c448968cc423a0112c26c147bd711a41dd74621
Deb Mendoza <debmendoza13@gmail.com>
2018-11-16 20:12:47 -0600

    Final running code

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#import time
-#from datetime import datetime
-#import pytz
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,14 +6,12 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://build-a-blog:root@localhost:8889/build-a-blog"
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-#central = pytz.timezone('America/Chicago')
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
-    #pub_date = db.Column(db.DateTime, default=datetime.datetime.now())
-
+    
     def __init__(self, title, body):
         self.title = title
         self.body = body
@@ -27,18 +22,14 @@ def index():
 
     if not id:
         listings = Blog.query.order_by(Blog.id.desc()).all()
-        #listings = Blog.query.order_by(Blog.pub_date.desc()).all()
-        #for listing in listings:
-            #listing.pub_date=datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
-            #pytz.utc.localize(listing.pub_date).astimezone(central)
+        
         return render_template("blog.html", title="Build a Blog", listings=listings)
     else:
         listing = Blog.query.filter_by(id=id).first()
         name = listing.title
         body = listing.body
-        #pubdate = pytz.utc.localize(listing.pub_date).astimezone(central)
-
-        return render_template("display_entry.html", name=name, body=body) #, pubdate=pubdate)
+        
+        return render_template("display_entry.html", name=name, body=body)
 
 @app.route("/newpost", methods=["POST"])
 def postform():
